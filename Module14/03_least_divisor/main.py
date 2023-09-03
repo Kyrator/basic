eb3c56a89e6bda8732d7619788d4bec9bce784bd
Aleksandr Syrysev <kyrator@bk.ru>
2023-09-03 23:56:52 +0300

    added func check(), func calculate(), func main()

--- Module14/03_least_divisor/main.py
+++ Module14/03_least_divisor/main.py
@@ -1,1 +1,25 @@
 # TODO здесь писать код
+
+
+def check(number):
+    while number < 2:
+        print("Введите число больше 1")
+        number = int(input("Введите число: "))
+    return number
+
+
+def calculate(number):
+    for i in range(2, number//2):
+        if number % i == 0:
+            number = i
+            break
+    return number
+
+def main():
+    number = int(input("Введите число: "))
+    number = check(number)
+    answer = calculate(number)
+    print(f"Наименьший делитель, отличный от единицы: {answer}")
+
+
+main()
