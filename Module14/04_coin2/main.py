75867e9d38bf21ff685bfe6aefd47bdd58eeb8fd
Aleksandr Syrysev <kyrator@bk.ru>
2023-09-05 20:35:00 +0300

    decided 4 task added func [check; coinPlace; checkCoin; main]

--- Module14/04_coin2/main.py
+++ Module14/04_coin2/main.py
@@ -1,1 +1,36 @@
 # TODO здесь писать код
+import math
+
+
+def check(number, id_point):
+    while number < 0:
+        print("Введите положительное число ")
+        if id_point == 'x':
+            number = float(input("X: "))
+        elif id_point == 'y':
+            number = float(input("Y: "))
+    return number
+
+
+def coinPlace(point_x, point_y):
+    return math.sqrt(point_x ** 2 + point_y ** 2)
+
+
+def checkCoin(radius_search, radius_coin):
+    if radius_search >= radius_coin:
+        print("Монетка где-то рядом")
+    else:
+        print("Монетки в области нет")
+
+
+def main():
+    print("Введите координаты монетки:")
+    point_x = check(float(input("X: ")), 'x')
+    point_y = check(float(input("Y: ")), 'y')
+    radius_search = float(input("Введите радиус: "))
+    radius_coin = coinPlace(point_x, point_y)
+    print(f"radius_coin = {radius_coin}")
+    checkCoin(radius_search, radius_coin)
+
+
+main()
