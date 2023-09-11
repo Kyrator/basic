1e91300a08bbc207fff9728e70d6601e88549af8
Aleksandr Syrysev <kyrator@bk.ru>
2023-09-11 22:14:52 +0300

    decided task7

--- Module15/07_word_analysis_2/main.py
+++ Module15/07_word_analysis_2/main.py
@@ -1,1 +1,13 @@
 # TODO здесь писать код
+
+word = input("Введите слово: ")
+count_case = 0
+count_word = len(word)
+for i in range(count_word // 2):
+    if word[i] == word[count_word - i - 1]:
+        count_case += 1
+        if count_case == count_word // 2:
+            print("Слово является палиндромом")
+    else:
+        print("Слово не является палиндромом")
+        break
