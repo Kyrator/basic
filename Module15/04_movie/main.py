25615efff0dd1c38484ece983c30388e8c1d15d9
Aleksandr Syrysev <kyrator@bk.ru>
2023-09-11 21:06:29 +0300

    decided task4

--- Module15/04_movie/main.py
+++ Module15/04_movie/main.py
@@ -3,3 +3,19 @@ films = ['Крепкий орешек', 'Назад в будущее', 'Так�
          'Мементо', 'Отступники', 'Деревня']
 
 # TODO здесь писать код
+
+favorite_films = []
+
+count_favorite_films = int(input("Сколько фильмов хотите добавить? "))
+
+while count_favorite_films:
+    film = input("Введите название фильма: ")
+    if film not in films:
+        print(f"Ошибка: фильма {film} у нас нет :(")
+    elif film not in favorite_films:
+        favorite_films.append(film)
+        count_favorite_films -= 1
+    else:
+        print(f"{film} уже есть в списке любимых.")
+
+print("Ваш список любимых фильмов: ", ", ".join(favorite_films))
