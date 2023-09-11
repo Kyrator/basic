1af8abe985263b59b3af4ba386461466bad1922b
Aleksandr Syrysev <kyrator@bk.ru>
2023-09-11 22:34:38 +0300

    decided task9 i don't know what i must write in this message. Only "decided" going to my mind

--- Module15/09_reverse_analysis/main.py
+++ Module15/09_reverse_analysis/main.py
@@ -2,3 +2,14 @@
 
 # Список чисел для работы (итоговый алгоритм проверьте на разных списках, придуманных самостоятельно):
 numbers_list = [7, 14, 3, 18, 21, 10, 9, 6]
+number_len = len(numbers_list)
+
+for index in range(number_len // 2 + 1):
+    numbers_list[index], numbers_list[number_len - index - 1] = numbers_list[number_len - index - 1], numbers_list[
+        index]
+
+for number in numbers_list:
+    if number % 2 != 0:
+        numbers_list.remove(number)
+
+print(numbers_list)
