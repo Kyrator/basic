668302ffc79977f715b659a89c3ce34ee7d09ac5
Aleksandr Syrysev <kyrator@bk.ru>
2023-09-11 22:13:38 +0300

    decided task6

--- Module15/06_running_nums/main.py
+++ Module15/06_running_nums/main.py
@@ -1,1 +1,13 @@
 # TODO здесь писать код
+
+old_list = [1, 4, -3, 0, 10]
+new_list = []
+
+move = int(input("Сдвиг: "))
+print("Изначальный список:", old_list)
+count_list = len(old_list)
+move = move % count_list  # don't jump behind border
+for i in range(count_list):
+    new_list.append(old_list[(i - move) % count_list])
+
+print('Сдвинутый список:', new_list)
