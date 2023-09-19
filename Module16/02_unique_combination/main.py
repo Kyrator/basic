c5c7090f1c1e312e3dbee6fc59ab91737a7faf3e
Aleksandr Syrysev <kyrator@bk.ru>
2023-09-19 20:19:58 +0300

    added def merge_sorted_lists

--- Module16/02_unique_combination/main.py
+++ Module16/02_unique_combination/main.py
@@ -1,4 +1,7 @@
-# TODO здесь писать код
+def merge_sorted_lists(list_1, list_2):
+    list_1.extend(list_2)
+    list_1 = set(list_1)
+    return sorted(list_1)
 
 
 # Пример использования:
@@ -6,3 +9,5 @@ list1 = [1, 3, 5, 7, 9]
 list2 = [2, 4, 5, 6, 8, 10]
 merged = merge_sorted_lists(list1, list2)
 print(merged)
+
+
