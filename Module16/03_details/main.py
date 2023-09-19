42f2cf3b9cc972473d192ecab58aee692c1e8481
Aleksandr Syrysev <kyrator@bk.ru>
2023-09-19 20:37:58 +0300

    decided task3 added def count_stuff and input stuff

--- Module16/03_details/main.py
+++ Module16/03_details/main.py
@@ -2,4 +2,20 @@ shop = [['каретка', 1200], ['шатун', 1000], ['седло', 300],
         ['педаль', 100], ['седло', 1500], ['рама', 12000],
         ['обод', 2000], ['шатун', 200], ['седло', 2700]]
 
+
 # TODO здесь писать код
+
+def count_stuff(search_stuff, array_goods):
+    goods_count = 0
+    goods_total_cost = 0
+    for i_goods in array_goods:
+        if i_goods[0] == search_stuff:
+            goods_count += 1
+            goods_total_cost += i_goods[1]
+    print("Кол-во деталей —", goods_count)  # 3
+    print("Общая стоимость —", goods_total_cost)  # 4500
+
+
+stuff = input("Название детали: ")  # седло
+
+count_stuff(stuff, shop)
