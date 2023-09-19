d35a43bf43447d0f7763e06d0396054c1e373c58
Aleksandr Syrysev <kyrator@bk.ru>
2023-09-19 20:16:32 +0300

    complite task1

--- Module16/01_scary_code/main.py
+++ Module16/01_scary_code/main.py
@@ -1,24 +1,20 @@
-a = [1, 5, 3]
-b = [1, 5, 1, 5]
-c = [1, 3, 1, 5, 3, 3]
-for i in b:
-    a.append(i)
-t = 0
-for i in a:
-    if i == 5:
-        t += 1
-print(t)
-d = []
-for i in a:
-    if i != 5:
-        d.append(i)
-for i in c:
-    d.append(i)
-t = 0
-for i in d:
-    if i == 3:
-        t += 1
-print(t)
-print(d)
+first = [1, 5, 3]
+second = [1, 5, 1, 5]
+third = [1, 3, 1, 5, 3, 3]
 
-# TODO переписать программу
+first.extend(second)
+print("Результат работы программы:")
+print("Количество цифр 5 при первом объединении: ", first.count(5))
+
+first = first[::-1]
+
+for i in first:
+    if i == 5:
+        first.remove(5)
+
+first = first[::-1]
+
+first.extend(third)
+
+print("Количество цифр 3 при первом объединении: ", first.count(3))
+print("Итоговый список:", first)
