f35dc9103721ce567c018a01334908bc5cbda997
Aleksandr Syrysev <kyrator@bk.ru>
2023-09-19 21:27:18 +0300

    decided task5 added input and for

--- Module16/05_songs/main.py
+++ Module16/05_songs/main.py
@@ -11,3 +11,15 @@ violator_songs = [
 ]
 
 # TODO здесь писать код
+count_song = int(input("Сколько песен выбрать? "))
+total_time_playlist = 0
+
+for i_song in range(count_song):
+    i_song += 1
+    question = "Название " + str(i_song) + "-й песни: "
+    answer = input(question)
+    for i_violator_songs in violator_songs:
+        if i_violator_songs[0] == answer:
+            total_time_playlist += i_violator_songs[1]
+
+print("\nОбщее время звучания песен:", round(total_time_playlist, 2), "минуты")
