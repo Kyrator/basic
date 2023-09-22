96b97c154426ed51686917fe5b044f06f2ace9b8
Aleksandr Syrysev <kyrator@bk.ru>
2023-09-22 19:19:31 +0300

    fixed variable n on length

--- Module16/08_simmetrical_seq/main.py
+++ Module16/08_simmetrical_seq/main.py
@@ -4,11 +4,11 @@ count_number = int(input("Количество чисел: "))
 temp_array = []
 array = [int(input("Число: ")) for _ in range(count_number)]
 
-n = len(array)
+length = len(array)
 count = 0
 
-for i in range(n):
-    for j in range(n - 1, i, -1):
+for i in range(length):
+    for j in range(length - 1, i, -1):
         if array[i] != array[j]:
             temp_array.insert(0, array[i])
             count += 1
