e2c9a87efee2ca2e83cbae85e67cd970a76eec89
Aleksandr Syrysev <kyrator@bk.ru>
2023-09-22 19:17:38 +0300

    decided task8

--- Module16/08_simmetrical_seq/main.py
+++ Module16/08_simmetrical_seq/main.py
@@ -1,1 +1,19 @@
 # TODO здесь писать код
+
+count_number = int(input("Количество чисел: "))
+temp_array = []
+array = [int(input("Число: ")) for _ in range(count_number)]
+
+n = len(array)
+count = 0
+
+for i in range(n):
+    for j in range(n - 1, i, -1):
+        if array[i] != array[j]:
+            temp_array.insert(0, array[i])
+            count += 1
+            break
+
+print("Последовательность: ", array)
+print("Нужно приписать чисел:", len(temp_array))
+print("Сами числа:", temp_array)
