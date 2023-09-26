55964b6ef1b7fcf7533d970f95b15f854c7ef583
Aleksandr Syrysev <kyrator@bk.ru>
2023-09-26 19:52:53 +0300

    fixed delete method slize wrote def reverse().

--- Module16/01_scary_code/main.py
+++ Module16/01_scary_code/main.py
@@ -1,3 +1,10 @@
+def reverse(elements):
+    temp = []
+    for element in elements:
+        temp.insert(0, element)
+    return temp
+
+
 first = [1, 5, 3]
 second = [1, 5, 1, 5]
 third = [1, 3, 1, 5, 3, 3]
@@ -6,13 +13,13 @@ first.extend(second)
 print("Результат работы программы:")
 print("Количество цифр 5 при первом объединении: ", first.count(5))
 
-first = first[::-1]
+first = reverse(first)
 
-for i in first:
-    if i == 5:
+for i_first in first:
+    if i_first == 5:
         first.remove(5)
 
-first = first[::-1]
+first = reverse(first)
 
 first.extend(third)
 
