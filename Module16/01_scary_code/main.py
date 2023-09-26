ad0c508cfbb7bac7ac4d69e9f1859ce1423d8b19
Aleksandr Syrysev <kyrator@bk.ru>
2023-09-26 23:44:30 +0300

    fixed task1, del def reverse, added def del_element, remove unneeded operations

--- Module16/01_scary_code/main.py
+++ Module16/01_scary_code/main.py
@@ -1,11 +1,12 @@
-def reverse(elements):
-    temp = []
-    for element in elements:
-        temp.insert(0, element)
-    return temp
+def del_element(array, element_del):
+    temp_del_element = []
+    for i_array in array:
+        if element_del != i_array:
+            temp_del_element.append(i_array)
+    return temp_del_element
 
 
-first = [1, 5, 5, 5, 3]
+first = [1, 5, 3]
 second = [1, 5, 1, 5]
 third = [1, 3, 1, 5, 3, 3]
 
@@ -13,15 +14,8 @@ first.extend(second)
 print("Результат работы программы:")
 print("Количество цифр 5 при первом объединении: ", first.count(5))
 
-first = reverse(first)
-
 # TODO: вы так не удалите все 5 из списка; подключите дебаггер, посмотрите, что происходит со списком в цикле
-for i_first in first:
-    if i_first == 5:
-        first.remove(5)
-
-first = reverse(first)
-
+first = del_element(first, 5)
 first.extend(third)
 
 print("Количество цифр 3 при первом объединении: ", first.count(3))
