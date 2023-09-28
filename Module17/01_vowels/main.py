1ea643e222a614aa5d48b0a450d1a80a506080e7
Serge K <ks7100313@gmail.com>
2023-09-28 23:31:41 +0300

    See comments

--- Module17/01_vowels/main.py
+++ Module17/01_vowels/main.py
@@ -3,3 +3,5 @@ line = input("Введите текст: ")
 list_vowels = [letter for letter in line if letter in "аоиеёэыуюя"]
 print("Список гласных букв: ", list_vowels)
 print("Длина списка:", len(list_vowels))
+
+# TODO: решаем в рамках пройденного материала, пожалуйста (методы списков)
