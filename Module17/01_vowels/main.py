081f0e5c6499c2bdd02e99291eb2d6a180075110
Kyrator <kyrator@bk.ru>
2023-10-01 13:26:26 +0300

    fixed task 8 added def check

--- Module17/01_vowels/main.py
+++ Module17/01_vowels/main.py
@@ -1,5 +1,6 @@
 # TODO здесь писать код
+vowes_alphabet = "аоиеёэыуюя"
 line = input("Введите текст: ")
-list_vowels = [letter for letter in line if letter in "аоиеёэыуюя"]
+list_vowels = [letter for letter in line for vowes in vowes_alphabet if letter == vowes ]
 print("Список гласных букв: ", list_vowels)
 print("Длина списка:", len(list_vowels))
--- Module17/08_caesar_cipher/main.py
+++ Module17/08_caesar_cipher/main.py
@@ -1,11 +1,20 @@
 # TODO здесь писать код
+def check_symbol (symbol):
+    answer = False
+    for letter in alphabet:
+        if letter == symbol:
+            answer = True
+    return answer
+
 def caeser_cipher(string, user_shift):
-    char_list = [(alphabet[(alphabet.index(sym) + user_shift) % 33] if sym != ' ' else ' ') for sym in string]
+    char_list = [(alphabet[(alphabet.index(sym) + user_shift) % 33]) 
+                 if check_symbol (sym) else sym
+                 for sym in string
+                 ]
     new_str = ''
     for i_char in char_list:
         new_str += i_char
     return new_str
-
 
 alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
 input_str = input('Введите сообщение: ')
