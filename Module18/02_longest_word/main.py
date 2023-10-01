a81df6bee4a725972a16b6458203b008498e93fd
Kyrator <kyrator@bk.ru>
2023-10-01 23:24:00 +0300

    decided task2

--- Module18/02_longest_word/main.py
+++ Module18/02_longest_word/main.py
@@ -1,1 +1,16 @@
 # TODO здесь писать код
+def max_words(words):
+    max_count = 0
+    word_max_words = ''
+    for i_word in words:
+        if len(i_word) > max_count:
+            max_count = len(i_word)
+            word_max_words = i_word
+    return word_max_words
+
+
+line_input = input('Введите строку: ').split()
+word = max_words(line_input)
+count_word = len(word)
+print('Самое длинное слово: \"{word}\" '.format(word=word))
+print("Длина этого слова: {count} символ.".format(count=count_word))
