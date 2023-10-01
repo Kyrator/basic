40c002be18e2af49499add3d141a9b61fa8922e6
Kyrator <kyrator@bk.ru>
2023-10-01 23:42:46 +0300

    decided task9

--- Module18/09_comment_analysis/main.py
+++ Module18/09_comment_analysis/main.py
@@ -1,5 +1,13 @@
 # TODO здесь писать код
-
+def count_uppercase_lowercase(text):
+    lower = 0
+    upper = 0
+    for letter in text:
+        if letter.islower():
+            lower += 1
+        elif letter.isupper():
+            upper += 1
+    return upper, lower
 
 # Пример использования:
 text = input("Введите строку для анализа: ")
