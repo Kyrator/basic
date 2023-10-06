6af175e7ce456f099f8756693e73a60b5c44a61c
Kyrator <kyrator@bk.ru>
2023-10-06 23:19:56 +0300

    fixed task_4

--- Module18/04_capital_letters/main.py
+++ Module18/04_capital_letters/main.py
@@ -1,4 +1,4 @@
 # TODO здесь писать код
 line_input = input('Введите строку: ').split()
 line_rezult = ' '.join(line_input).title()
-print("Результат: {line_rezult}".format(line_rezult = line_rezult))
+print('Результат: {line_rezult}'.format(line_rezult=line_rezult))
