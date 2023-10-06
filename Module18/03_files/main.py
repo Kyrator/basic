ee7d7bf260610d68d207dcfe00a53b1e749be6c5
Kyrator <kyrator@bk.ru>
2023-10-06 23:18:08 +0300

    fixed task_3

--- Module18/03_files/main.py
+++ Module18/03_files/main.py
@@ -4,6 +4,6 @@ if line_input.startswith(("@", "№", "$", "%", "^", "&", "*", "(", ")")):
     print("Ошибка: название начинается недопустимым символом.")
 else:
     if line_input.endswith(".txt") or line_input.endswith(".docx"):
-        print("Файл назван верно.")// 
+        print("Файл назван верно.")
     else:
         print("Ошибка: неверное расширение файла. Ожидалось .txt или .docx.")
