e5d31bd861e84a443f0b18265ad526e6c482df47
Kyrator <kyrator@bk.ru>
2023-10-08 12:56:06 +0300

    fixed task_4 stayed only title()

--- Module18/04_capital_letters/main.py
+++ Module18/04_capital_letters/main.py
@@ -1,6 +1,5 @@
 # TODO здесь писать код
-line_input = input('Введите строку: ').split()
-line_rezult = ' '.join(line_input).title()
-print('Результат: {line_rezult}'.format(line_rezult=line_rezult))
+line_input = input('Введите строку: ').title()
+print('Результат: {line_rezult}'.format(line_rezult=line_input))
 
 # TODO: .title()
