289c445e0f7387e0bfd557c83a4ca95e8ae2cfa6
Kyrator <kyrator@bk.ru>
2023-10-08 12:29:15 +0300

    fixed task_1 change split ans join on replace

--- Module18/01_restaurant_menu/main.py
+++ Module18/01_restaurant_menu/main.py
@@ -1,6 +1,5 @@
 # TODO здесь писать код
-input_line = input('Доступное меню: ').split(';')
-output_line = ', '.join(input_line)
-print(output_line)
+input_line = input('Доступное меню: ').replace(';',', ')
+print(input_line)
 
 # TODO: .replace()
