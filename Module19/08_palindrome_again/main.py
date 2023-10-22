683afbceb917641ffa2d605e3cb2c14aa7b385dd
kyrator <kyrator@bk.ru>
2023-10-22 15:41:45 +0300

    decided task8

--- Module19/08_palindrome_again/main.py
+++ Module19/08_palindrome_again/main.py
@@ -1,1 +1,17 @@
 # TODO здесь писать код
+def func_1(line):
+    for i_line in range(len(line)):
+        temp = (line[i_line::-1] + line[:i_line:-1])
+        if temp == line:
+            print("Можно сделать палиндромом")
+            break
+    else:
+        print("Нельзя сделать палиндромом")
+
+
+def main():
+    line = input("Введите строку: ")
+    func_1(line)
+
+
+main()
