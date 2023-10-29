07ad139db379a94af8b5d6dead34d95d5da6d86f
Александр Сырысев <kyrator@bk.ru>
2023-10-29 22:08:56 +0300

    fixed task5

--- Module19/05_synonym_dict/main.py
+++ Module19/05_synonym_dict/main.py
@@ -1,37 +1,19 @@
 # TODO здесь писать код
 
 
-# Проверка слова в словаре
-def request_word():
-    word_request = input("Введите слово: ").lower()
-    if library_dict.get(word_request, False):
-        print(library_dict.get(word_request))
-    else:
-        not_exist_word()
-
-
-# Если слова нет в словаре
-def not_exist_word():
-    print("Такого слова в словаре нет.")
-    request_word()
-
-
 library_dict = dict()
-i_count_couple = 1
 count_couple = int(input("Введите количество пар слов: "))
-while i_count_couple <= count_couple:
-    line_input = input(f"{i_count_couple} пара: ").lower().split('-')
-    line_strip = [word.strip() for word in line_input]
-    if len(line_strip) == 2:
-        synonym_0, synonym_1 = line_strip
-        library_dict[synonym_0] = synonym_1
-        library_dict[synonym_1] = synonym_0
-        i_count_couple += 1
+
+for i_count_couple in range(1, count_couple + 1):
+    line_input = input(f"{i_count_couple} пара: ").title().split(' — ')
+    synonym_0, synonym_1 = line_input
+    library_dict[synonym_0] = synonym_1
+    library_dict[synonym_1] = synonym_0
+
+while True:
+    word_request = input("Введите слово: ").title()
+    if library_dict.get(word_request):
+        print(library_dict.get(word_request))
+        break
     else:
-        print("Некорректный ввод ")
-        print("Попробуйте заново")
-
-request_word()
-
-# TODO: synonym_0: synonym_1, synonym_1: synonym_0
-# .get() -- решите в одно обращение задачу поиска 
+        print("Такого слова в словаре нет.")
