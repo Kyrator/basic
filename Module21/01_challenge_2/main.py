db3af63bd844565d7c8a31477884e2a8f80361ab
Александр Сырысев <kyrator@bk.ru>
2023-11-03 11:05:46 +0300

    fixed task1

--- Module21/01_challenge_2/main.py
+++ Module21/01_challenge_2/main.py
@@ -1,11 +1,12 @@
 # TODO здесь писать код
 def recursive(value):
-  if value  == 1:
-    print(value)
+    if value == 1:
+        print(value)
 
-  else:
-    recursive(value-1)
-    print(value)
+    else:
+        recursive(value - 1)
+        print(value)
+
 
 number_till = int(input("Введите num: "))
 print()
