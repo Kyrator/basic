4cebd42fcdcae933628c0ab622c26db962be913f
Александр Сырысев <kyrator@bk.ru>
2023-11-03 20:53:42 +0300

    decided task4

--- Module21/04_advanced_sum/main.py
+++ Module21/04_advanced_sum/main.py
@@ -1,1 +1,17 @@
 # TODO здесь писать код
+
+def sum_data(*args):
+    summa_data = 0
+    for value in args:
+        if isinstance(value, (dict, list)):
+            summa_data += sum_data(*value)
+        else:
+            summa_data += value
+    return summa_data
+
+
+summa1 = sum_data([[1, 2, [3]], [1], 3])
+print("Ответ в консоли: {summa}".format(summa=summa1))
+
+summa2 = sum_data(1, 2, 3, 4, 5)
+print("Ответ в консоли: {summa}".format(summa=summa2))
