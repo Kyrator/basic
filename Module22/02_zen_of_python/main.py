529dfae2861c1a85e3fa48ea25fcd5b24c189d60
Александр Сырысев <kyrator@bk.ru>
2023-11-05 21:49:26 +0300

    decided task2

--- Module22/02_zen_of_python/main.py
+++ Module22/02_zen_of_python/main.py
@@ -1,1 +1,8 @@
 # TODO здесь писать код
+file = 'zen.txt'
+
+file_zen = open(file, 'r', encoding='UTF-8')
+text = [i_line.strip() for i_line in file_zen]
+for i_text in text[::-1]:
+    print(i_text)
+file_zen.close()
