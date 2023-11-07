909016b012a2f0b6326d2d62b9b1f7845005b5c1
Александр Сырысев <kyrator@bk.ru>
2023-11-07 08:47:14 +0300

    fixed task1

--- Module22/01_nums_sum_2/main.py
+++ Module22/01_nums_sum_2/main.py
@@ -5,20 +5,20 @@ summa = 0
 
 
 print("Содержимое файла {file}".format(file=file_input))
-file_input_text = open(file_input, 'r', encoding='UTF-8')
-for i_line in file_input_text:
-    print(i_line, end='')
-    for i_elem in i_line:
-        if i_elem.isdigit():
-            summa += int(i_elem)
-file_input_text.close()
-print()
-file_output_text = open(file_output, 'w', encoding='UTF-8')
-file_output_text.write(str(summa))
-file_output_text.close()
+with open(file_input, 'r', encoding='UTF-8') as file_input_text:
+    for i_line in file_input_text:
+        print(i_line, end='')
+        for i_elem in i_line:
+            if i_elem.isdigit():
+                summa += int(i_elem)
+    file_input_text.close()
+    print()
+    file_output_text = open(file_output, 'w', encoding='UTF-8')
+    file_output_text.write(str(summa))
+    file_output_text.close()
 
-print("Содержимое файла {file}".format(file=file_output))
-file_input_text = open(file_output, 'r', encoding='UTF-8')
-for i_out_line in file_input_text:
-    print(i_out_line)
-file_output_text.close()
+    print("Содержимое файла {file}".format(file=file_output))
+    file_input_text = open(file_output, 'r', encoding='UTF-8')
+    for i_out_line in file_input_text:
+        print(i_out_line)
+
