763629cd6c0cbea702b2834735715afc3baba475
Александр Сырысев <kyrator@bk.ru>
2023-11-16 10:51:59 +0300

    fixed task1

--- Module24/01_fight/main.py
+++ Module24/01_fight/main.py
@@ -3,39 +3,33 @@ import random
 
 
 class Fighter:
-    def __init__(self, index):
-        self.index = index
-        self.name = "Воин {}".format(self.index)
+    def __init__(self, name, number):
+        self.name = name
+        self.number = number
         self.health = 100
 
 # TODO: методы класса должны быть в классе; например, дать в нос, все еще жив
 # внешний код должен организовать "битву"
+    def hit(self, fighter):
+        fighter.health -= 20
+        print(f"Ударил {self.name}, {fighter.name} потерял 20 ед. здоровья.")
+        print(f" У {self.name}а осталось {self.health}. У {fighter.name}а осталось {fighter.health}.\n")
 
 
-class Ring:
-    status_battle = True
+fighter_1 = Fighter("Васян", 1)
+fighter_2 = Fighter("Колян", 2)
+fight = [fighter_1, fighter_2]
 
-    def __init__(self):
-        self.warriors = [Fighter(index) for index in range(1, 3)]
-
-    def battle(self):
+while True:
+    if fighter_1.health * fighter_2.health:
         choose_warrior = random.randint(0, 1)
-        self.warriors[(choose_warrior + 1) % 2].health -= 20
-        print("Ударил {}, {} потерял 20 ед. здоровья. Осталось {}".format(
-            self.warriors[choose_warrior].name,
-            self.warriors[(choose_warrior + 1) % 2].name,
-            self.warriors[(choose_warrior + 1) % 2].health
-        ))
-        self.status_battle = self.warriors[(choose_warrior + 1) % 2].health * self.warriors[
-            (choose_warrior + 1) % 2].health
-        if not self.status_battle:
-            win = ''
-            for warrior in self.warriors:
-                if warrior.health != 0:
-                    win = warrior.name
-            print("Выйграл {} ".format(win))
+        if choose_warrior:
+            fighter_1.hit(fighter_2)
+        else:
+            fighter_2.hit(fighter_1)
+    else:
+        win = fighter_1.name if fighter_1.health > 0 else fighter_2.name
+        print("Битва закончилась. Победил {} ".format(win))
+        break
 
 
-fight = Ring()
-while fight.status_battle:
-    fight.battle()
