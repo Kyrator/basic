f001b2165238710782c4f82af1004c0d41c3ddeb
Александр Сырысев <kyrator@bk.ru>
2023-11-21 16:08:23 +0300

    fixed task5

--- Module25/05_stack/main.py
+++ Module25/05_stack/main.py
@@ -2,54 +2,42 @@
 class Stack:
 
     def __init__(self):
-        self.lst_task = []
+        self.__lst_task = []
 
-    def add_task(self, task):
-        self.lst_task.append(task)
+    def push(self, task):
+        self.__lst_task.append(task)
 
-    def delete_task(self, task):
-        self.lst_task.remove(task)
+    def pop(self, task):
+        if len(self.__lst_task) == 0:
+            return None
+        self.__lst_task.pop()
 
     def __str__(self):
-        return f"{' '.join(self.lst_task)}"
+        return "; ".join(self.__lst_task)
 
 
 class TaskManager:
 
-    def __init__(self, dct_task=None):
-        if dct_task is None:
-            dct_task = {}
-        self.dct_task = dct_task
+    def __init__(self):
+        self.dct_task = dict()
 
     def new_task(self, task, key):
-        if not self.dct_task.get(key):
+        if key not in self.dct_task:
             self.dct_task[key] = Stack()
-            self.dct_task.setdefault(key, []).add_task(task)
-        else:
-            if task in str(self.dct_task[key]):
-                key += 1
-                self.dct_task[key] = Stack()
-            self.dct_task.setdefault(key, []).add_task(task)
+        self.dct_task[key].push(task)
 
-    def remove_task(self, task):
-        for key, val in self.dct_task.items():
-            if task in str(val):
-                self.dct_task[key].delete_task(task)
-                break
-        else:
-            print(f'Задачи - {task} в списке приоритетов нет')
 
-        copy_dct = self.dct_task.copy()
-        for key in copy_dct.keys():
-            if len(str(copy_dct[key])) == 0:
-                self.dct_task.pop(key)
 
     def __str__(self):
-        answer = ""
-        for key, val in sorted(self.dct_task.items()):
-            answer += f"{key}: {val};"
-            answer += "\n"
-        return answer
+        response = []
+        if self.dct_task:
+            for key in sorted(self.dct_task.keys()):
+                response.append("{key} {value} \n".format(
+                    key=key,
+                    value=self.dct_task[key]
+                )
+            )
+        return ''.join(response)
 
 
 
