369b856daa5a7dc7551646dff1b225d428f77b37
Александр Сырысев <kyrator@bk.ru>
2023-12-03 01:32:43 +0300

    decided task2

--- Module27/02_slowdown/main.py
+++ Module27/02_slowdown/main.py
@@ -1,1 +1,36 @@
-# TODO здесь писать код
+import functools
+from typing import Callable
+import time
+
+
+def slower(func: Callable) -> None:
+    """ Декоратор """
+
+    @functools.wraps(func)
+    def wrapper() :
+        """
+        Обертка, которая перед выполнением
+        декорируемой функции ждёт
+        несколько секунд.
+        """
+        print("ждём несколько секунд")
+        time.sleep(3)
+        func()
+
+    return wrapper
+
+
+@slower
+def check_web_site() -> None:
+    """
+    Функция, которая постоянно проверяет,
+    изменились ли данные на веб-странице или её код
+    """
+    print("Проверка сайта")
+
+
+count = 1
+
+while count < 10:
+    check_web_site()
+    count += 1
