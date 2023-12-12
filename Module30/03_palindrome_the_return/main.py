d4dd75b0bc0f2fab85971f3af5ca37f49cfe236f
Александр Сырысев <kyrator@bk.ru>
2023-12-12 22:34:19 +0300

    decided task3

--- Module30/03_palindrome_the_return/main.py
+++ Module30/03_palindrome_the_return/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+from collections import Counter
+
+
+def can_be_poly(line: str) -> bool:
+    return len(line) % 2 == sum(x % 2 for x in Counter(line).values())
+
+
+print(can_be_poly('abcba'))  # Counter({'a': 2, 'b': 2, 'c': 2})
+print(can_be_poly('abbbc'))  # Counter({'b': 3, 'c': 2, 'a': 1})
