d72832c25b438415f2084e37c78e3775ab7fb30f
Александр Сырысев <kyrator@bk.ru>
2023-12-13 17:16:52 +0300

    decided task5

--- Module31/05_web_scrapping/main.py
+++ Module31/05_web_scrapping/main.py
@@ -1,6 +1,13 @@
 # TODO здесь писать код
+from re import findall
+
+
 
 # В данном случае запрос request.get заменен на загрзку сайта из файла html
 with open('examples.html', 'r') as f:
     text = f.read()
-# По итогу вы так же получаете код сайта в виде одной строки
+    result_beta = findall(r'>.+</h3>', text)
+    release = list(map(lambda x: x[1:-5], result_beta))
+    print(release)
+
+
